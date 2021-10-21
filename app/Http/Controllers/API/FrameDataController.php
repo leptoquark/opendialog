eaa58b5af2cf6f2ff1e8fa0b5e8a3e18b7aa0bb1
Stuart Haigh <stuart.haigh@opendialog.ai>
2021-10-21 10:21:01 +0100

    moves the frame data controller into the correct name space

--- app/Http/Controllers/API/FrameDataController.php
+++ app/Http/Controllers/API/FrameDataController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Responses\IncomingAvailableIntentsFrame;
 use App\Http\Responses\ContextResponse;
 use App\Http\Responses\IncomingSelectionFrame;
