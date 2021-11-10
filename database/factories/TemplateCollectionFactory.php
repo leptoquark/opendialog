f005203790258a225be181bf53d238241835dfa5
Stuart Haigh <stuart.haigh@opendialog.ai>
2021-11-10 12:34:58 +0000

    updates to new style factory

--- database/factories/TemplateCollectionFactory.php
+++ database/factories/TemplateCollectionFactory.php
@@ -1,21 +1,39 @@
 <?php
 
-use App\Template;
-use Faker\Generator as Faker;
+namespace Database\Factories;
 
-$factory->define(\App\TemplateCollection::class, function (Faker $faker) {
-    return [
-        'name' => $faker->unique()->words(3, true),
-        'description' => [
-            $faker->unique()->words(10, true),
-            $faker->unique()->words(10, true),
-            $faker->unique()->words(10, true)
-        ],
-        'preview' => [
-            'url' =>  $faker->url,
-            'selected_scenario' => $faker->hexColor,
-            'token' => $faker->uuid
-        ],
-        'active' => true,
-    ];
-});
+use App\TemplateCollection;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TemplateCollectionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TemplateCollection::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->words(3, true),
+            'description' => [
+                $this->faker->unique()->words(10, true),
+                $this->faker->unique()->words(10, true),
+                $this->faker->unique()->words(10, true)
+            ],
+            'preview' => [
+                'url' =>  $this->faker->url,
+                'selected_scenario' => $this->faker->hexColor,
+                'token' => $this->faker->uuid
+            ],
+            'active' => true,
+        ];
+    }
+}
