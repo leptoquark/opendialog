c84a2a964b09123627fe5751c22d6eb5b2b2b369
Stuart Haigh <stuart.haigh@opendialog.ai>
2021-11-15 17:22:07 +0000

    moves the validation for creation_type outside of the `if` block

--- app/Http/Requests/ScenarioCreateRequest.php
+++ app/Http/Requests/ScenarioCreateRequest.php
@@ -18,11 +18,11 @@ class ScenarioCreateRequest extends ScenarioRequest
         if ($this->query('creation_type', 'default') === 'default') {
             $rules['od_id'][] = 'required';
             $rules['name'][] = 'required';
-
-            $creationType = $this->query('creation_type');
-            $rules['creation_type'] = ['nullable', 'string', 'in:default,duplicate,from-template'];
-            $rules['object_id'] = [Rule::requiredIf(!is_null($creationType) && $creationType !== 'default')];
         }
+
+        $creationType = $this->query('creation_type');
+        $rules['creation_type'] = ['nullable', 'string', 'in:default,duplicate,from-template'];
+        $rules['object_id'] = [Rule::requiredIf(!is_null($creationType) && $creationType !== 'default')];
 
         return $rules;
     }
